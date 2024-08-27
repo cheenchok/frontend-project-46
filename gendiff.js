e524280a8351e4942396b4a73904480db5597261
cheenchok <cheenchok@gmail.com>
2024-08-27 12:59:53 +0300

    feat: name and description in gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -1,1 +1,8 @@
 import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1');
+
+program.parse(process.argv);
