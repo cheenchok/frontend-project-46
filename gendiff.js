8b924a4ee20566d3542f0d69eb2ab3215de4de94
cheenchok <cheenchok@gmail.com>
2024-08-27 13:06:59 +0300

    feat: option and arguments in gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -3,6 +3,8 @@ import { program } from 'commander';
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse(process.argv);
