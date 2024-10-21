7a37098e77804c00059776f323b586fa2d19b503
cheenchok <cheenchok@gmail.com>
2024-10-21 16:25:48 +0300

    fix: change main in package.json

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,2 @@
 #!/usr/bin/env node
-import { program } from 'commander';
-import { genDiff } from '../src/gendiff.js';
-
-program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
-  .option('-f, --format [type]', 'output format')
-  .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2, options) => {
-    console.log(genDiff(filepath1, filepath2, options.format));
-  });
-
-program.parse();
+import '../src/index.js';
