0e848a0412b1c53e73e54b7cb3422af7e83c2a05
cheenchok <cheenchok@gmail.com>
2024-11-27 07:43:06 +0300

    fix: plain diff output

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,7 +1,8 @@
 import { DIFF_TYPES } from '../consts.js';
 import { isObject } from '../utils.js';
 
-const formatValue = (value) => (isObject(value) ? '[complex value]' : value);
+const formatValue = (value) =>
+  isObject(value) ? '[complex value]' : `${value}`;
 
 const getFullPath = (parentPath, key) =>
   parentPath ? `${parentPath}.${key}` : key;
@@ -9,21 +10,13 @@ const getFullPath = (parentPath, key) =>
 const makeLine = (key, postfix) => `Property '${key}' was ${postfix}`;
 
 export function plain(data, parentPath, nested) {
+  console.log(data);
   const result = data
     .map(({ diffType, key, value }, i) => {
-      const prevKeyEqual = key === data[i - 1]?.key;
-      const nextKeyEqual = key === data[i - 2]?.key;
-      const isUpdated = prevKeyEqual || nextKeyEqual;
-
-      if (isUpdated) {
-        if (prevKeyEqual) return null;
-
-        return makeLine(
-          getFullPath(parentPath, key),
-          `updated. From ${formatValue(value)} to ${formatValue(
-            data[i - 2].value
-          )}`
-        );
+      const prevItem = data[i - 1];
+      const nextItem = data[i + 1];
+      if (prevItem?.key === key) {
+        return null;
       }
 
       switch (diffType) {
@@ -33,7 +26,14 @@ export function plain(data, parentPath, nested) {
             `added with value: ${formatValue(value)}`
           );
         case DIFF_TYPES.ABSENT:
-          return makeLine(getFullPath(parentPath, key), `removed`);
+          return nextItem?.key === key
+            ? makeLine(
+                getFullPath(parentPath, key),
+                `updated. From ${formatValue(value)} to ${formatValue(
+                  nextItem.value
+                )}`
+              )
+            : makeLine(getFullPath(parentPath, key), `removed`);
         case DIFF_TYPES.NESTED:
           return plain(value, getFullPath(parentPath, key), true);
         case DIFF_TYPES.EQUALITY:
