8609753e27dea24f34bee8206cdb54a55f80dfe7
cheenchok <cheenchok@gmail.com>
2024-11-27 08:13:11 +0300

    style: gendiff

--- src/gendiff.js
+++ src/gendiff.js
@@ -3,58 +3,70 @@ import { isObject } from './utils.js';
 import { DIFF_TYPES } from './consts.js';
 import { formatter } from './formatters/index.js';
 
+function getSortedKeys(...objList) {
+  const merged = objList.reduce((acc, item) => Object.assign(acc, item), {});
+  return Object.keys(merged).sort();
+}
+
+function makeDiffItem(key, value, diffType) {
+  return {
+    key,
+    value,
+    diffType,
+  };
+}
+
 function genDiffFromObj(data1, data2, isNested = false) {
-  const mergeData = { ...data1, ...data2 };
-  const keys = Object.keys(mergeData).sort();
+  const keys = getSortedKeys(data1, data2);
 
   return keys
     .map((key) => {
       if (key in data1 && key in data2) {
         if (data1[key] === data2[key])
-          return { key, value: data1[key], diffType: DIFF_TYPES.EQUALITY };
+          return makeDiffItem(key, data1[key], DIFF_TYPES.EQUALITY);
 
         if (isObject(data1[key]) && isObject(data2[key]))
-          return {
+          return makeDiffItem(
             key,
-            value: genDiffFromObj(data1[key], data2[key], isNested),
-            diffType: DIFF_TYPES.NESTED,
-          };
+            genDiffFromObj(data1[key], data2[key], isNested),
+            DIFF_TYPES.NESTED
+          );
 
         return [
-          {
+          makeDiffItem(
             key,
-            value: isObject(data1[key])
+            isObject(data1[key])
               ? genDiffFromObj(data1[key], {}, true)
               : data1[key],
-            diffType: isNested ? DIFF_TYPES.NESTED : DIFF_TYPES.ABSENT,
-          },
-          {
+            isNested ? DIFF_TYPES.NESTED : DIFF_TYPES.ABSENT
+          ),
+          makeDiffItem(
             key,
-            value: isObject(data2[key])
+            isObject(data2[key])
               ? genDiffFromObj(data2[key], {}, true)
               : data2[key],
-            diffType: isNested ? DIFF_TYPES.NESTED : DIFF_TYPES.EXTRA,
-          },
+            isNested ? DIFF_TYPES.NESTED : DIFF_TYPES.EXTRA
+          ),
         ];
       }
 
       if (key in data1)
-        return {
+        return makeDiffItem(
           key,
-          value: isObject(data1[key])
+          isObject(data1[key])
             ? genDiffFromObj(data1[key], {}, true)
             : data1[key],
-          diffType: isNested ? DIFF_TYPES.NESTED : DIFF_TYPES.ABSENT,
-        };
+          isNested ? DIFF_TYPES.NESTED : DIFF_TYPES.ABSENT
+        );
 
       if (key in data2)
-        return {
+        return makeDiffItem(
           key,
-          value: isObject(data2[key])
+          isObject(data2[key])
             ? genDiffFromObj(data2[key], {}, true)
             : data2[key],
-          diffType: isNested ? DIFF_TYPES.NESTED : DIFF_TYPES.EXTRA,
-        };
+          isNested ? DIFF_TYPES.NESTED : DIFF_TYPES.EXTRA
+        );
     })
     .flat();
 }
