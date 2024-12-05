f5be5b972a1662ae98fc829f112ab835a6ae5fa0
cheenchok <cheenchok@gmail.com>
2024-12-05 13:54:25 +0300

    test: fix fixtures result

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,45 +1,34 @@
 import fs from 'fs';
 import genDiff from '../src/gendiff.js';
 
-const makeFileNames = (format, isFlat) =>
-  Array.from(
-    Array(2),
-    (_, i) =>
-      `__fixtures__/${isFlat ? 'flat' : 'nested'}-file${i + 1}.${format}`
-  );
+const pathPrefix = '__fixtures__/';
+
+const file1JsonPath = `${pathPrefix}file1.json`;
+const file1YmlPath = `${pathPrefix}file1.yml`;
+const file2JsonPath = `${pathPrefix}file2.json`;
+const file2YmlPath = `${pathPrefix}file2.yml`;
+const resultPlain = fs.readFileSync(`${pathPrefix}result_plain.txt`, 'utf8');
+const resultStylish = fs.readFileSync(
+  `${pathPrefix}result_stylish.txt`,
+  'utf8'
+);
 
 describe('gendiff', () => {
-  const result = {
-    flat: {
-      stylish:
-        '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}',
-      plain:
-        "Property 'follow' was removed\nProperty 'proxy' was removed\nProperty 'timeout' was updated. From 50 to 20\nProperty 'verbose' was added with value: true",
-      json: fs.readFileSync('__fixtures__/diff/flat-json', 'utf8'),
-    },
-    nested: {
-      stylish: fs.readFileSync('__fixtures__/diff/nested-stylish', 'utf8'),
-      plain: fs.readFileSync('__fixtures__/diff/nested-plain', 'utf8'),
-      json: fs.readFileSync('__fixtures__/diff/nested-json', 'utf8'),
-    },
-  };
+  test('plain with yml', () => {
+    expect(genDiff(file1YmlPath, file2YmlPath, 'plain')).toEqual(resultPlain);
+  });
 
-  ['stylish', 'plain', 'json']
-    .flatMap((formatter) => [
-      { formatter, isFlat: false },
-      { formatter, isFlat: true },
-    ])
-    .flatMap((item) => [
-      { ...item, format: 'json' },
-      { ...item, format: 'yml' },
-    ])
-    .forEach(({ formatter, format, isFlat }) =>
-      test(`${formatter} formatter for ${
-        isFlat ? 'flat' : 'nested'
-      } ${format}`, () => {
-        expect(genDiff(...makeFileNames(format, isFlat), formatter)).toEqual(
-          result[isFlat ? 'flat' : 'nested'][formatter]
-        );
-      })
+  test('plain with json', () => {
+    expect(genDiff(file1JsonPath, file2JsonPath, 'plain')).toEqual(resultPlain);
+  });
+
+  test('stylish with yml', () => {
+    expect(genDiff(file1YmlPath, file2YmlPath, 'plain')).toEqual(resultStylish);
+  });
+
+  test('stylish with json', () => {
+    expect(genDiff(file1JsonPath, file2JsonPath, 'plain')).toEqual(
+      resultStylish
     );
+  });
 });
