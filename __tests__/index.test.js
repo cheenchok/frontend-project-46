0b7799171dba8f3725e84aed8a30afe79179893f
cheenchok <cheenchok@gmail.com>
2024-12-30 10:58:56 +0300

    style: fix eslint error in test 'describe' is not defined

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,6 @@
 import fs from 'fs';
+// eslint is not configured correctly in hexlet workflow, jest is not added to env
+import { describe, expect, test } from '@jest/globals';
 import genDiff from '../src/gendiff.js';
 
 const pathPrefix = '__fixtures__/';
@@ -8,10 +10,7 @@ const file1YmlPath = `${pathPrefix}file1.yml`;
 const file2JsonPath = `${pathPrefix}file2.json`;
 const file2YmlPath = `${pathPrefix}file2.yml`;
 const resultPlain = fs.readFileSync(`${pathPrefix}result_plain.txt`, 'utf8');
-const resultStylish = fs.readFileSync(
-  `${pathPrefix}result_stylish.txt`,
-  'utf8',
-);
+const resultStylish = fs.readFileSync(`${pathPrefix}result_stylish.txt`, 'utf8');
 
 describe('gendiff', () => {
   test('plain with yml', () => {
@@ -23,14 +22,10 @@ describe('gendiff', () => {
   });
 
   test('stylish with yml', () => {
-    expect(genDiff(file1YmlPath, file2YmlPath, 'stylish')).toEqual(
-      resultStylish,
-    );
+    expect(genDiff(file1YmlPath, file2YmlPath, 'stylish')).toEqual(resultStylish);
   });
 
   test('stylish with json', () => {
-    expect(genDiff(file1JsonPath, file2JsonPath, 'stylish')).toEqual(
-      resultStylish,
-    );
+    expect(genDiff(file1JsonPath, file2JsonPath, 'stylish')).toEqual(resultStylish);
   });
 });
